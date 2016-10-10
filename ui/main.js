a3cf53e9f12e2da85bcfc46ab408bd6bd52848a0
Karthika N. J <karthika.nj@gmail.com>
2016-10-10 11:47:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,10 @@
 console.log('Loaded!');
 
 var element = document.getElementById('main-text');
-element.innerHTML=("Hello. Now text is changed through java script :) ");
+element.innerHTML="Hello. Now text is changed through java script :) ";
+
+//To move the pic 
+var image = document.getElementById('madi');
+image.onclick = function () {
+    image.style.marginleft='100px';
+}
