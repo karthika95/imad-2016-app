b08faf391ba698e9dff6b4d746192748fad56822
Karthika N. J <karthika.nj@gmail.com>
2016-10-10 11:55:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,12 @@ element.innerHTML="Hello. Now text is changed through java script :) ";
 
 //To move the pic 
 var image = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight() {
+    marginLeft+=10;
+    image.style.marginLeft= marginLeft +'px';
+}
 image.onclick = function() {
+    var interval = setInterval(moveRight,100);
     image.style.marginleft='100px';
 }
